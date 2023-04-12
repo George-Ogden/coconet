26763a0f395147c704653ad04cedfae7fa45b7ee
George Ogden <george@goprogram.ai>
2023-04-12 16:25:23 +0100

    train with diffusers

--- train.py
+++ train.py
@@ -1,7 +1,9 @@
+from diffusers import DDPMPipeline, DDPMScheduler, UNet2DModel
+from accelerate import Accelerator
+
 import torch.utils.data as data
 import torch.nn.functional as F
 import torch.nn as nn
-import numpy as np
 import torch
 
 from copy import copy
@@ -12,8 +14,6 @@ from dataclasses import dataclass
 import os
 
 from data.dataset import Jsb16thSeparatedDataset, Jsb16thSeparatedDatasetFactory, DatasetInfo
-from model import Model
-
 
 @dataclass
 class TrainingConfig:
@@ -21,10 +21,26 @@ class TrainingConfig:
     batch_size: int = 16
     learning_rate: float = 1e-4
     diffusion_timesteps: int = 300
-    min_beta: float = 1e-4
-    max_beta: float = 2e-2
+    beta_schedule: str = "linear"
+    prediction_type: str = "epsilon"
     save_directory: str = "checkpoints"
 
+def _extract_into_tensor(arr, timesteps, broadcast_shape):
+    """
+    Extract values from a 1-D numpy array for a batch of indices.
+
+    :param arr: the 1-D numpy array.
+    :param timesteps: a tensor of indices into the array to extract.
+    :param broadcast_shape: a larger shape of K dimensions with the batch
+                            dimension equal to the length of timesteps.
+    :return: a tensor of shape [batch_size, 1, ...] where the shape has K dims.
+    """
+    if not isinstance(arr, torch.Tensor):
+        arr = torch.from_numpy(arr)
+    res = arr[timesteps].float().to(timesteps.device)
+    while len(res.shape) < len(broadcast_shape):
+        res = res[..., None]
+    return res.expand(broadcast_shape)
 
 class Trainer:
     def __init__(
@@ -33,116 +49,142 @@ class Trainer:
         config: TrainingConfig=TrainingConfig(),
         dataset_config: DatasetInfo=DatasetInfo()
     ):
-        # setup device
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
         # save config and model
-        self.model = model.to(self.device)
+        self.model = model
         self.config = copy(config)
         self.timesteps = config.diffusion_timesteps
         self.save_directory = config.save_directory
 
-        # precompute betas
-        betas = torch.linspace(config.min_beta, config.max_beta, config.diffusion_timesteps)
-        alphas = 1. - betas
-        alphas_cumprod = torch.cumprod(alphas, axis=0)
-        alphas_cumprod_prev = F.pad(alphas_cumprod[:-1], (1, 0), value=1.0)
-
-        self.sqrt_alphas_cumprod = torch.sqrt(alphas_cumprod).to(self.device)
-        self.sqrt_one_minus_alphas_cumprod = torch.sqrt(1. - alphas_cumprod).to(self.device)
-
-        self.posterior_variance = (betas * (1. - alphas_cumprod_prev) / (1. - alphas_cumprod)).to(self.device)
+        # setup noise scheduler
+        self.noise_scheduler = DDPMScheduler(
+            num_train_timesteps=self.config.diffusion_timesteps,
+            beta_schedule=self.config.beta_schedule,
+            prediction_type=self.config.prediction_type,
+        )
 
         # split into train and val
         self.dataset_config = copy(dataset_config)
 
-    def compute_loss(self, batch: torch.Tensor) -> torch.Tensor:
-        # sample timestep
-        t = torch.randint(0, self.timesteps, size=(len(batch),)).to(self.device)
-
-        # convert distribution to [-1, 1]
-        batch = (batch.to(self.device).float().permute((0, 3, 2, 1)) * .5) * 2
-        noise = torch.randn_like(batch)
-        noisy_batch = batch * self.sqrt_alphas_cumprod[t][:, np.newaxis, np.newaxis, np.newaxis] + self.sqrt_one_minus_alphas_cumprod[t][:, np.newaxis, np.newaxis, np.newaxis] * noise
-        
-        # compute loss
-        predicted = self.model(noisy_batch, t)
-        predicted = torch.clip(predicted, -1, 1)
-        loss = F.mse_loss(predicted, batch)
-        return loss
-
-    @torch.no_grad()
-    def generate_samples(self, batch_size: int) -> torch.Tensor:
-        distribution = torch.randn(
-            batch_size,
-            self.dataset_config.num_instruments,
-            self.dataset_config.num_pitches,
-            self.dataset_config.piece_length,
-            device=self.device,
-            dtype=torch.float32,
+        # setup accelerator
+        self.accelerator = Accelerator(
+            log_with="wandb",
+            logging_dir=config.save_directory
         )
-        for t in tqdm(
-            reversed(range(self.timesteps)), desc="Generating", total=self.timesteps
-        ):
-            # create distribution
-            distribution += torch.sqrt(self.posterior_variance[t]) * torch.randn_like(distribution)
-            distribution = self.model(distribution, t)
-        return distribution > 0
 
-    def train(self, train_dataset: Jsb16thSeparatedDataset, val_dataset: Jsb16thSeparatedDataset):
-        wandb.init(project="coconet", config=self.config, dir=self.save_directory)
-        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.config.learning_rate)
+    def train(self, train_dataset: Jsb16thSeparatedDataset):
+        optimizer = torch.optim.SGD(self.model.parameters(), lr=self.config.learning_rate)
 
-        # set up dataloaders
-        self.train_loader = data.DataLoader(
+        train_dataloader = data.DataLoader(
             train_dataset, batch_size=self.config.batch_size, shuffle=True, num_workers=4
         )
-        self.val_loader = data.DataLoader(
-            [val_dataset[i] for i in range(len(val_dataset))],
-            batch_size=self.config.batch_size,
-            shuffle=False,
+
+        self.model, optimizer, train_dataloader = self.accelerator.prepare(
+            self.model, optimizer, train_dataloader
         )
 
+        run = os.path.split(__file__)[-1].split(".")[0]
+        self.accelerator.init_trackers(run)
+
+        global_step = 0
         for epoch in range(self.config.epochs):
-            # train
             self.model.train()
-            train_loss = 0.
-            for batch in tqdm(self.train_loader, desc="Training", total=len(self.train_loader)):
-                optimizer.zero_grad()
-                loss = self.compute_loss(batch)
-                train_loss += loss.detach().cpu().item()
-                loss.backward()
-                optimizer.step()
-            train_loss /= len(self.train_loader)
+            progress_bar = tqdm(train_dataloader, total=len(train_dataloader))
+            progress_bar.set_description(f"Epoch {epoch}")
+            for batch in progress_bar:
 
-            # validate
-            self.model.eval()
-            val_loss = 0.
-            with torch.no_grad(), torch.random.fork_rng(devices=[self.device]):
-                # randomly seed for repeatable loss
-                torch.random.manual_seed(0)
-                for batch in tqdm(self.val_loader, desc="Validating", total=len(self.val_loader)):
-                    val_loss += self.compute_loss(batch).item()
-            val_loss /= len(self.val_loader)
+                # convert correct shape
+                clean_images = batch.float().permute((0, 3, 2, 1))
+                # Sample noise that we'll add to the images
+                noise = torch.randn(clean_images.shape).to(clean_images.device)
+                bsz = clean_images.shape[0]
+                # Sample a random timestep for each image
+                timesteps = torch.randint(
+                    0, self.config.diffusion_timesteps, (bsz,), device=clean_images.device
+                ).long()
 
-            # generate samples
-            samples = self.generate_samples(4)
+                # Add noise to the clean images according to the noise magnitude at each timestep
+                # (this is the forward diffusion process)
+                noisy_images = self.noise_scheduler.add_noise(clean_images, noise, timesteps)
 
-            # save
-            os.makedirs(f"{self.save_directory}/{epoch:04d}/samples/", exist_ok=True)
-            torch.save(model, f"{self.save_directory}/{epoch:04d}/model.pth")
-            for i, sample in enumerate(samples):
-                self.dataset_config.save_pianoroll(sample.cpu().permute((2, 1, 0)).numpy(), f"{self.save_directory}/{epoch:04d}/samples/{i:02d}.mid")
-            wandb.log({"train_loss": train_loss, "val_loss": val_loss})
+                with self.accelerator.accumulate(self.model):
+                    # Predict the noise residual
+                    model_output = self.model(noisy_images, timesteps).sample
 
+                    if self.config.prediction_type == "epsilon":
+                        loss = F.mse_loss(model_output, noise)  # this could have different weights!
+                    elif self.config.prediction_type == "sample":
+                        alpha_t = _extract_into_tensor(
+                            self.noise_scheduler.alphas_cumprod, timesteps, (clean_images.shape[0], 1, 1, 1)
+                        )
+                        snr_weights = alpha_t / (1 - alpha_t)
+                        loss = snr_weights * F.mse_loss(
+                            model_output, clean_images, reduction="none"
+                        )  # use SNR weighting from distillation paper
+                        loss = loss.mean()
+                    else:
+                        raise ValueError(f"Unsupported prediction type: {self.config.prediction_type}")
+
+                    self.accelerator.backward(loss)
+
+                    optimizer.step()
+                    optimizer.zero_grad()
+                    global_step += 1
+
+                logs = {"loss": loss.detach().item(), "step": global_step}
+                progress_bar.set_postfix(**logs)
+                self.accelerator.log(logs, step=global_step)
+
+            # Generate sample images for visual inspection
+            unet = self.accelerator.unwrap_model(self.model)
+
+            pipeline = DDPMPipeline(
+                unet=unet,
+                scheduler=self.noise_scheduler,
+            )
+
+            generator = torch.Generator(device=pipeline.device).manual_seed(0)
+            # run pipeline in inference (sample random noise and denoise)
+            images = pipeline(
+                generator=generator,
+                batch_size=10,
+                num_inference_steps=self.config.diffusion_timesteps,
+                output_type="numpy",
+            ).images
+
+            # denormalize the images and save to tensorboard
+            images_processed = (images * 255).round().astype("uint8")
+
+            # Upcoming `log_images` helper coming in https://github.com/huggingface/accelerate/pull/962/files
+            self.accelerator.get_tracker("wandb").log(
+                {"test_samples": [wandb.Image(img) for img in images_processed], "epoch": epoch},
+                step=global_step,
+            )
+
+            # save the model
+            unet = self.accelerator.unwrap_model(self.model)
+
+            pipeline = DDPMPipeline(
+                unet=unet,
+                scheduler=self.noise_scheduler,
+            )
+
+            pipeline.save_pretrained(self.config.save_directory)
+            for i, sample in enumerate(images_processed):
+                self.dataset_config.save_pianoroll((sample > 0).transpose((2, 1, 0)), f"{self.save_directory}/{epoch:04d}-{i:02d}.mid")
+
+        self.accelerator.end_training()
 
 if __name__ == "__main__":
     factory = Jsb16thSeparatedDatasetFactory()
-    model = Model(factory.info)
+    model = UNet2DModel(
+        in_channels=factory.info.num_instruments,
+        out_channels=factory.info.num_instruments,
+        sample_size=(factory.info.num_pitches, factory.info.piece_length),
+    )
     trainer = Trainer(
         model,
         config=TrainingConfig(),
         dataset_config=factory.info,
     )
 
-    trainer.train(factory.train_dataset, factory.val_dataset)
+    trainer.train(factory.train_dataset)
